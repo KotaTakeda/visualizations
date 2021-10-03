80e6384f89032964ee5d58849f1c6d73834c283f
Kota Takeda <takeda_kl@icloud.com>
2021-10-03 19:50:46 +0900

    mobjects

---
+++ manim/project/mobjects.py
@@ -0,0 +1,72 @@
+from manim import *
+
+# creting and displaying -------------------
+class CreatingMobjects(Scene):
+    def construct(self):
+        circle = Circle()
+        self.add(circle)
+        self.wait(1)
+        self.remove(circle)
+        self.wait(1)
+
+# placing ----------------------------------
+class PlacingMobjects(Scene):
+    def construct(self):
+        # creating
+        circle = Circle()
+        square = Square()
+        triangle = Triangle()
+
+        # placing
+        # shift one unit in the UP/LEFT/RIGHT direction
+        circle.shift(LEFT)
+        square.shift(UP)
+        triangle.shift(RIGHT)
+
+        # add/display
+        self.add(circle, square, triangle)
+        self.wait(1)
+
+class MobjectPlacement(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+        triangle = Triangle()
+
+        # place the circle two units left from the origin
+        circle.move_to(LEFT * 2)
+        # place the square to the left of the circle
+        square.next_to(circle, LEFT)
+        # align the left border of the triangle to the left border of the circle
+        triangle.align_to(circle, LEFT)
+
+        self.add(circle, square, triangle)
+        self.wait(1)
+
+# styling Mobjects
+class MobjectStyling(Scene):
+    def construct(self):
+        circle = Circle().shift(LEFT)
+        square = Square().shift(UP)
+        triangle = Triangle().shift(RIGHT)
+
+        circle.set_stroke(color=GREEN, width=20)
+        square.set_fill(YELLOW, opacity=1.0)
+        triangle.set_fill(PINK, opacity=0.5)
+
+        self.add(circle, square, triangle)
+        self.wait(1)
+
+class MobjectZOrder(Scene):
+    def construct(self):
+        circle = Circle().shift(LEFT)
+        square = Square().shift(UP)
+        triangle = Triangle().shift(RIGHT)
+
+        circle.set_stroke(color=GREEN, width=20)
+        square.set_fill(YELLOW, opacity=1.0)
+        triangle.set_fill(PINK, opacity=0.5)
+
+        # addの順番でz-indexを調整
+        self.add(triangle, square, circle)
+        self.wait(1)
