e072267551bcf660ba0a3d91d2ea3a70f4d09aac
Kota Takeda <takeda_kl@icloud.com>
2021-10-15 07:52:20 +0900

    text完了

--- manim/project/text.py
+++ manim/project/text.py
@@ -31,3 +31,72 @@ class DifferentWeight(Scene):
         for weight in weight_list:
             g += Text(weight.name, weight=weight.name, font="Open Sans")
         self.add(g.arrange(DOWN).scale(0.5))
+
+# Using Colors
+class SimpleColor(Scene):
+    def construct(self):
+        col = Text("RED COLOR", color=RED)
+        self.add(col)
+
+# sliceして色を指定
+class Textt2cExample(Scene):
+    def construct(self):
+        t2cindices = Text('Hello', t2c={'[1:-1]': BLUE}).move_to(LEFT)
+        t2cwords = Text('World',t2c={'rl':RED}).next_to(t2cindices, RIGHT)
+        self.add(t2cindices, t2cwords)
+
+# Using Gradients
+class GradientExample(Scene):
+    def construct(self):
+        t = Text("Hello", gradient=(RED, BLUE, GREEN), font_size=96)
+        self.add(t)
+
+class t2gExample(Scene):
+    def construct(self):
+        t2gindices = Text(
+            'Hello',
+            t2g={
+                '[1:-1]': (RED,GREEN),
+            },
+        ).move_to(LEFT)
+        t2gwords = Text(
+            'World',
+            t2g={
+                'World':(RED,BLUE),
+            },
+        ).next_to(t2gindices, RIGHT)
+        self.add(t2gindices, t2gwords)
+
+# Setting Line Spacing
+class LineSpacing(Scene):
+    def construct(self):
+        a = Text("Hello\nWorld", line_spacing=1)
+        b = Text("Hello\nWorld", line_spacing=4)
+        self.add(Group(a,b).arrange(LEFT, buff=5))
+
+# Disabling Ligatures
+# 合字の無効化．'ae'などが使えなくなる．
+class DisableLigature(Scene):
+    def construct(self):
+        li = Text("fl ligature",font_size=96)
+        nli = Text("fl ligature", disable_ligatures=True, font_size=96)
+        self.add(Group(li, nli).arrange(DOWN, buff=.8))
+
+# Iterating
+class IterateColor(Scene):
+    def construct(self):
+        text = Text("Colors", font_size=96)
+        for letter in text:
+            letter.set_color(random_bright_color())
+        self.add(text)
+
+# Working with MarkupText
+# MarkupTextはhtmlに似たPangoMarkup形式を処理できる．
+class MarkupTest(Scene):
+    def construct(self):
+        text = MarkupText(
+            f'<span underline="double" underline_color="green">double green underline</span> in red text<span fgcolor="{YELLOW}"> except this</span>',
+            color=RED,
+            font_size=34
+        )
+        self.add(text)
